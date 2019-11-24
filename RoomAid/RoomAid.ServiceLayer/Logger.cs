6e856dce06f987e0ecb857ad7cc91881e415dd15
michellkuang <48337104+michellkuang@users.noreply.github.com>
2019-11-23 17:09:16 -0800

    GetCallingMethod()

---
+++ RoomAid/RoomAid.ServiceLayer/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomAid.ServiceLayer
+{
+    public class Logger
+    {
+        public static void Log()
+        {
+
+        }
+
+        private static MethodBase GetCallingMethod()
+        {
+            var stackTrace = new StackTrace();
+            var currentMethod = MethodBase.GetCurrentMethod();
+            for (var i = 0; i < stackTrace.FrameCount; i++)
+            {
+                var methodName = stackTrace.GetFrame(i).GetMethod().Name;
+                if ((!methodName.Equals("Log")) && (!methodName.Equals(currentMethod.Name)))
+                {
+                    var stackFrame = new StackFrame(i, true);
+                    return stackFrame.GetMethod();
+                }
+            }
+            return currentMethod;
+        }
+    }
+}
