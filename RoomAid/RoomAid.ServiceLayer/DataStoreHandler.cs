53ec1e6b5f3076b868c4de099ffeb07e7d341605
Nightsfallen <44853027+Nightsfallen@users.noreply.github.com>
2019-11-24 21:23:51 -0800

    DSHandler

--- RoomAid/RoomAid.ServiceLayer/DataStoreHandler.cs
+++ RoomAid/RoomAid.ServiceLayer/DataStoreHandler.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace RoomAid.ServiceLayer
 {
-    internal class DataStoreHandler : ILogHandler
+    class DataStoreHandler : ILogHandler
     {
-        public bool WriteLog(LogMessage logMessage)
-        {
-            return false;
-        }
-
         public bool DeleteLog(LogMessage logMessage)
         {
-            return false;
+            return true;
+        }
+
+        public bool WriteLog(LogMessage logMessage)
+        {
+            var client = new MongoClient("mongodb+srv://<rwUser>:<readwrite>@logs-s3nyt.gcp.mongodb.net/test?retryWrites=true&w=majority");
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<BsonDocument>("Logs");
+
+            var document = new BsonDocument
+            {
+                {"DateTime", BsonValue.Create(logMessage.Time) },
+                {"Class",BsonValue.Create(logMessage.CallingClass)},
+                {"Method",BsonValue.Create(logMessage.CallingMethod) },
+                {"Level",BsonValue.Create(logMessage.Level) },
+                {"UserID",BsonValue.Create(logMessage.UserID) },
+                {"SessionID",BsonValue.Create(logMessage.SessionID) },
+                {"Text",BsonValue.Create(logMessage.Text) }
+            };
+            collection.InsertOne(document);
+            return true;
         }
     }
 }
