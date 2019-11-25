03650335dca94e94c9f8cbcf43cfecbd7565ca18
MichellKuang <michellkuang96@yahoo.com>
2019-11-24 17:42:12 -0800

    Added Handler and LogDAO files (need to fix)

---
+++ RoomAid/RoomAid.ServiceLayer/FileHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomAid.ServiceLayer
+{
+    internal class FileHandler : ILogHandler
+    {
+        public bool WriteLog(LogMessage logMessage)
+        {
+            return false;
+        }
+
+        public bool DeleteLog(LogMessage logMessage)
+        {
+            return false;
+        }
+    }
+}
---
+++ RoomAid/RoomAid.ServiceLayer/LogDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomAid.ServiceLayer
+{
+    class LogDAO
+    {
+        public static bool WriteLog(LogMessage logMessage)
+        {
+            try
+            {
+                DataStoreHandler DSHandler = new DataStoreHandler();
+                FileHandler FHandler = new FileHandler();
+                DSHandler.WriteLog(logMessage);
+                FHandler.WriteLog(logMessage);
+            }
+            catch (Exception e)
+            {
+                // TODO: Delete log entry that was stored
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
