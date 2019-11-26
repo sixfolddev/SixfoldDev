13d49bf49e4e3be3eafb06d6657b0486aec1bd58
alber <albertdu233@gmail.com>
2019-11-26 12:43:04 -0800

    add code

---
+++ ServiceLayer/ArchiveService.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+
+namespace RoomAid.ServiceLayer.Service
+{
+    public class ArchiveService
+    {
+        private string logStorageDirectory;
+        private string archiveStorageDirectory;
+        private int logLife;
+        private string dateFormat;
+        private string cultureInfo;
+        private DriveInfo driveOfArchive;
+        private double allocatedSpace;//Assuem we always leave 0.5 GB for a minmum allocated space
+        private string sevenZipPath;
+        private int timeOfRetry;
+        //Constructor
+        public ArchiveService()
+        {
+            logStorageDirectory = @"D:\LogStorage\";
+            archiveStorageDirectory = @"D:\ArchiveStorage\";
+            logLife = 30;
+            dateFormat = "yyyyMMdd";
+            cultureInfo = "en-US";
+            driveOfArchive = new DriveInfo("D");
+            allocatedSpace = 250000000;
+            sevenZipPath = @"D:\7-Zip\7z.exe";
+            timeOfRetry = 3;
+            
+        }
+        /// <summary>
+        /// Method Archiveable will check the given filename to see if this log file is old enough to be
+        /// archived. 
+        /// </summary>
+        /// <param name="fileName">log file that being checked</param>
+        /// <returns>True if it is old enough, False if it is not</returns>
+        public bool Archiveable(string fileName)
+        {
+            string logDate = fileName.Substring(0, 8);
+            DateTime.TryParseExact(logDate, dateFormat, new CultureInfo(cultureInfo),
+                DateTimeStyles.None, out DateTime logDateTime);
+            if ((DateTime.UtcNow - logDateTime).TotalDays > logLife)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// Method GetFileNames will go through the log storage, check all log files and collect log files that are
+        /// old enough to be archive into a list.
+        /// </summary>
+        /// <returns>resultSet the list of log files that should be archived</returns>
+        public List<string> GetFileNames()
+        {
+           var resultSet = new List<string>();
+            foreach (var logFile in Directory.GetFiles(logStorageDirectory))
+            {
+                string fileName = Path.GetFileName(logFile);
+                if (Archiveable(fileName) == true)
+                {
+                    resultSet.Add(fileName);
+                }
+            }
+            
+            return resultSet;
+        }
+
+        public bool IsSpaceEnough(DriveInfo driveOfArchive, double requiredSpace)
+        {
+            if (driveOfArchive.AvailableFreeSpace < requiredSpace || !driveOfArchive.IsReady)
+            {
+                
+                return false;
+            }
+            return true;
+        }
+        public bool RunArchive()
+        {
+            //Before any step of archive started, the space check is required, if the free space is
+            //not less than the allocated space, or the drive is not available, the archive process should not start
+            //The admin shall be notified and this archive period shall be logged as failure
+            if(IsSpaceEnough(driveOfArchive, allocatedSpace) == false)
+            {
+                //log.createLog(failure);
+                //notify admin
+                return false;
+            }
+            //Before start the archive, system need to make sure that 7z.exe is installed in the machine
+            if (File.Exists(sevenZipPath) == false)
+            {
+                return false;
+            }
+            var resultSet = new List<string>(); 
+            resultSet = GetFileNames();
+            //if the result set is empty, the archive process must be stopped.
+            if (resultSet.Count == 0)
+            {
+                //log.createLog(failure);
+                return false;
+            }
+            //Only if all steps were oeprated succeffully, the archive process could return true
+            return true;
+        }
+        public bool FileOutPut(List<string> resultSet)
+        {
+            bool ifSuccess = true;
+            string deleteFailed = @"D:\LogStorage\";
+            string compressFailed = @"D:\LogStorage\";
+            foreach (string fileName in resultSet)
+            {
+
+                bool compressSuccess = AddToSevenZip(fileName);
+
+                if (compressSuccess == false)
+                {
+                    for (int i = 0; i< timeOfRetry;i++)
+                    {
+                        compressSuccess = AddToSevenZip(fileName);
+                        if (compressSuccess == true)
+                        {
+                            ifSuccess = true;
+                            break;
+                            //Notify admin with compressFailed
+                        }
+                       
+                    }
+                    if(compressSuccess == false)
+                    {
+                        compressFailed = compressFailed + fileName;
+                        ifSuccess = false;
+                    } 
+                }   
+                if (compressSuccess == true)
+                {
+                    bool deleteSuccess = DeleteLog(fileName);
+                    if(deleteSuccess == false)
+                    {
+                        for (int i = 0; i < timeOfRetry; i++)
+                        {
+                            deleteSuccess = DeleteLog(fileName);
+                            if (deleteSuccess == true)
+                            {
+                                ifSuccess = true;
+                                break;
+                                //Notify admin with compressFailed
+                            }
+
+                        }
+                        if (deleteSuccess == false)
+                        {
+                            deleteFailed = deleteFailed + fileName;
+                            ifSuccess = false;
+                        }
+                    }
+                }
+               
+            }
+            return ifSuccess;
+        }
+
+        public bool AddToSevenZip(string fileName)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = sevenZipPath;
+                string outPutFilePath = archiveStorageDirectory + DateTime.Now.ToString(dateFormat) +
+                    ".7z";
+                string filePath = logStorageDirectory + fileName;
+                process.StartInfo.Arguments = " a -t7z " + outPutFilePath + " " + filePath + "";
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.Start();
+                process.WaitForExit();
+                process.Close();
+            }
+            catch (Exception e)
+            {
+                //log.createLog(failure);
+               
+                return false;
+            }
+            return true;
+
+        }
+
+        public bool DeleteLog(string fileName)
+        {
+            string filePath = logStorageDirectory + fileName;
+            try
+            {
+                if (File.Exists(filePath) == false)
+                {
+                    return false;
+                }
+                File.Delete(filePath);
+            }
+            catch (Exception e)
+            {
+                //log.createLog(failure);
+                return false;
+            }
+            return true;
+
+        }
+    }
+}
---
+++ UnitTest/ArchiveTest.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoomAid.ServiceLayer.Service;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ArchiveTest
+    {
+       
+        [TestMethod]
+        ///<summary>
+        ///Test for log archiveable, a log file that is old enough should pass
+        ///</summary>
+        public void IsNotOldPass()
+        {
+            //Arrange
+            ArchiveService archiver = new ArchiveService();
+            bool expected = false;
+            //Act
+            bool actual = archiver.Archiveable("20191119.csv");
+            Console.WriteLine("the result is:"+actual);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        ///<summary>
+        ///Test for log archiveable, a log file that is not old enough should not pass
+        ///</summary>
+        [TestMethod]
+        public void IsOldPass()
+        {
+            //Arrange
+            ArchiveService archiver = new ArchiveService();
+            bool expected = true;
+            //Act
+            bool actual = archiver.Archiveable("20190919.csv");
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        ///<summary>
+        ///Test for GetFileNames() to see if it can return a list correctly
+        ///</summary>
+        [TestMethod]
+        public void ListNotEmptyPassA()
+        {
+            //Arrange
+            ArchiveService archiver = new ArchiveService();
+            bool expected = true;
+            bool actual = false;
+            //Act
+            System.IO.File.WriteAllText(@"D:\LogStorage\20190826.csv", "testing");
+            System.IO.File.WriteAllText(@"D:\LogStorage\20190824.csv", "testing");
+            List<string> resultSet = archiver.GetFileNames();
+            if (resultSet.Count > 0)
+            {
+                actual =true;
+            }
+            archiver.DeleteLog("20190826.csv");
+            archiver.DeleteLog("20190824.csv");
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        ///<summary>
+        ///Test for GetFileNames() to see if it can return a list correctly
+        ///An empty storage should return an empty list
+        ///</summary>
+        [TestMethod]
+        public void ListNotEmptyPassB()
+        {
+            //Arrange
+            ArchiveService archiver = new ArchiveService();
+            bool expected = false;
+            bool actual = false;
+            //Act
+            List<string> resultSet = archiver.GetFileNames();
+            if (resultSet.Count > 0)
+            {
+                actual = true;
+            }
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        ///<summary>
+        ///Test for DeleteLog() the method won't delete a file that does not exist
+        ///</summary>
+        [TestMethod]
+        public void DeleteNotPassA()
+        {
+            //Arrange
+            ArchiveService archiver = new ArchiveService();
+            bool expected = false;
+            //Act
+            bool actual = archiver.DeleteLog("FileNotExist.csv");
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        ///<summary>
+        ///Test for DeleteLog() the method won't delete a file twice
+        ///</summary>
+        [TestMethod]
+        public void DeleteNotPassB()
+        {
+            //Arrange
+            ArchiveService archiver = new ArchiveService();
+            bool expected = false;
+            //Act
+            System.IO.File.WriteAllText(@"D:\LogStorage\20190801.csv", "testing");
+            archiver.DeleteLog("20190801.csv");
+            bool actual = archiver.DeleteLog("20190801.csv");
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        ///<summary>
+        ///Test for DeleteLog() the method should delete a file correctly if the file path is 
+        ///correct
+        ///</summary>
+        [TestMethod]
+        public void DeletePass()
+        {
+            //Arrange
+            ArchiveService archiver = new ArchiveService();
+            bool expected = true;
+            //Act
+            System.IO.File.WriteAllText(@"D:\LogStorage\20190825.csv", "testing");
+            bool actual = archiver.DeleteLog("20190825.csv");
+            Console.WriteLine("the result is:" + actual);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OutPutPass()
+        {
+            //Arrange
+            ArchiveService archiver = new ArchiveService();
+            bool expected = true;
+            System.IO.File.WriteAllText(@"D:\LogStorage\20190815.csv", "testing");
+            var resultSet = archiver.GetFileNames();
+            //Act
+            bool actual = archiver.FileOutPut(resultSet);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    
+    }
+}
+
