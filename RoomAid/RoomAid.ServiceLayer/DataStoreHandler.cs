edb88a935a74125243a6b0a39b78dcf67c1ff4ec
Nightsfallen <44853027+Nightsfallen@users.noreply.github.com>
2019-11-26 14:00:08 -0800

    DsHandler

--- RoomAid/RoomAid.ServiceLayer/DataStoreHandler.cs
+++ RoomAid/RoomAid.ServiceLayer/DataStoreHandler.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,35 +8,71 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace RoomAid.ServiceLayer
-=======
-﻿namespace RoomAid.ServiceLayer
->>>>>>> 4d61057e7258b4cbbc69cb18b1832a8a028b16a4
 {
     class DataStoreHandler : ILogHandler
     {
-        public bool DeleteLog(LogMessage logMessage)
-        {
-            return true;
-        }
+        // <summary>
+        // Connects to the database and the collection specified by the time value stored within the log message
+        // Creates a new document and writes it to the collection
+        // <\summary>
 
+        // TODO: Write Asynch Database Write
         public bool WriteLog(LogMessage logMessage)
         {
-            var client = new MongoClient("mongodb+srv://<rwUser>:<readwrite>@logs-s3nyt.gcp.mongodb.net/test?retryWrites=true&w=majority");
-            var database = client.GetDatabase("test");
-            var collection = database.GetCollection<BsonDocument>("Logs");
-
-            var document = new BsonDocument
+            for(var i = 0; i < 4; i++)
             {
-                {"DateTime", BsonValue.Create(logMessage.Time) },
-                {"Class",BsonValue.Create(logMessage.CallingClass)},
-                {"Method",BsonValue.Create(logMessage.CallingMethod) },
-                {"Level",BsonValue.Create(logMessage.Level) },
-                {"UserID",BsonValue.Create(logMessage.UserID) },
-                {"SessionID",BsonValue.Create(logMessage.SessionID) },
-                {"Text",BsonValue.Create(logMessage.Text) }
-            };
-            collection.InsertOne(document);
-            return true;
+                try
+                {
+                    string collectionName = "Mongo_" + logMessage.Time.toString("yyyyMMdd");
+                    var client = new MongoClient("mongodb+srv://<rwUser>:<readwrite>@logs-s3nyt.gcp.mongodb.net/test?retryWrites=true&w=majority");
+                    var database = client.GetDatabase("test");
+                    var collection = database.GetCollection<BsonDocument>(collectionName);
+                    var document = new BsonDocument
+                    {
+                        {"LogID",BsonValue.Create(logMessage.LogGUID) },
+                        {"DateTime", BsonValue.Create(logMessage.Time) },
+                        {"Class",BsonValue.Create(logMessage.CallingClass)},
+                        {"Method",BsonValue.Create(logMessage.CallingMethod) },
+                        {"Level",BsonValue.Create(logMessage.Level) },
+                        {"UserID",BsonValue.Create(logMessage.UserID) },
+                        {"SessionID",BsonValue.Create(logMessage.SessionID) },
+                        {"Text",BsonValue.Create(logMessage.Text) }
+                    };
+                    collection.InsertOne(document);
+                    return true;
+                }
+                //TODO: Call error handling exception handler
+                catch (Exception e)
+                {
+                    if (i == 3)
+                    {
+                        throw e;
+                    }
+                }
+            }
+        }
+        public bool DeleteLog(LogMessage logMessage)
+        {
+            for (var i = 0; i < 4; i++)
+            {
+                try
+                {
+                    string collectionName = "Mongo_" + logMessage.Time.toString("yyyyMMdd");
+                    var client = new MongoClient("mongodb+srv://<rwUser>:<readwrite>@logs-s3nyt.gcp.mongodb.net/test?retryWrites=true&w=majority");
+                    var database = client.GetDatabase("test");
+                    var collection = database.GetCollection<BsonDocument>(collectionName);
+                    collection.FindOneAndDelete(Builders<BsonDocument>).Filter.Eq("LogID", logMessage.LogGUID);
+                }
+                //TODO: Error Handling exception handler
+                catch(Exception e)
+                {
+                    if (i == 3)
+                    {
+                        throw e;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
