1d1e7ae347ff9285a1961f12547a76224dc2e2d2
Nightsfallen <44853027+Nightsfallen@users.noreply.github.com>
2019-11-26 13:18:46 -0800

    Message

--- RoomAid/RoomAid.ServiceLayer/FileHandler.cs
+++ RoomAid/RoomAid.ServiceLayer/FileHandler.cs
@@ -3,34 +3,107 @@ using System.Text;
 
 namespace RoomAid.ServiceLayer
 {
-    internal class FileHandler : ILogHandler
+    public class FileHandler : ILogHandler
     {
         private readonly string _directory;
-        private readonly ILogFormatter formatter;
+        private readonly ILogFormatter _formatter;
 
+        /// <summary>
+        /// Default constructor. Initializes a directory to store the log files and the formatter to
+        /// format the log message.
+        /// </summary>
         public FileHandler()
         {
-            _directory = @"C:\LogFiles"; // Temporary directory
-            formatter = new SingleLineFormatter();
+            _directory = @"C:\SixfoldLogFiles"; // Temporary directory
+            _formatter = new SingleLineFormatter();
         }
 
+        // TODO: Write async
+        /// <summary>
+        /// Writes a log entry to a .csv file. If it fails, tries again up to 3x before throwing an exception.
+        /// </summary>
+        /// <param name="logMessage">Log entry to be written to file</param>
+        /// <returns>true if write is successful, false otherwise</returns>
         public bool WriteLog(LogMessage logMessage)
         {
-            var directory = new DirectoryInfo(_directory);
-            if (!directory.Exists)
+            for (var i = 0; i < 4; i++)
             {
-                directory.Create();
-            }
-            string path = Path.Combine(_directory, logMessage.Time.ToString("yyyyMMdd"), ".csv");
-            using (var writer = new StreamWriter(path, true, Encoding.UTF8)) // UTF8 Encoding recommended for .NET Framework 4.7.2
-            {
-                writer.WriteLine(formatter.FormatLog(logMessage)); // TODO: Write async
+                try
+                {
+                    var directory = new DirectoryInfo(_directory);
+                    if (!directory.Exists)
+                    {
+                        directory.Create();
+                    }
+                    string fileName = logMessage.Time.ToString("yyyyMMdd") + ".csv";
+                    string path = Path.Combine(_directory, fileName);
+                    StreamWriter writer;
+                    if (!File.Exists(path)) // If file doesn't exist, create and write parameter names as first line
+                    {
+                        using (writer = new StreamWriter(path, false, Encoding.UTF8))
+                        {
+                            writer.WriteLine(logMessage.GetParamNames());
+                        }
+                    }
+                    using (writer = new StreamWriter(path, true, Encoding.UTF8)) // UTF8 Encoding recommended for .NET Framework 4.7.2
+                    {
+                        writer.WriteLine(_formatter.FormatLog(logMessage));
+                    }
+                    writer.Close();
+                    return true;
+                }
+                catch (IOException e)
+                {
+                    if (i == 3)
+                    {
+                        throw e;
+                    }
+                }
             }
             return false;
         }
 
+        /// <summary>
+        /// Searches if a log entry exists in a .csv file and deletes it. If it fails, tries again up to 3x 
+        /// before throwing an exception.
+        /// </summary>
+        /// <param name="logMessage">Log entry to search for and delete</param>
+        /// <returns>true if delete is successful, false otherwise</returns>
         public bool DeleteLog(LogMessage logMessage)
         {
+            for (var i = 0; i < 4; i++)
+            {
+                try
+                {
+                    string fileName = logMessage.Time.ToString("yyyyMMdd") + ".csv";
+                    string path = Path.Combine(_directory, fileName);
+                    using (var reader = new StreamReader(path))
+                    {
+                        string line;
+                        StreamWriter writer = new StreamWriter(path, false, Encoding.UTF8);
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] tokens = line.Split(',');
+                            if (!(tokens[0].Equals(logMessage.LogGUID.ToString())))
+                            {
+                                using (writer)
+                                {
+                                    writer.WriteLine(line);
+                                }
+                            }
+                        }
+                        writer.Close();
+                    }
+                    return true;
+                }
+                catch (IOException e)
+                {
+                    if (i == 3)
+                    {
+                        throw e;
+                    }
+                }
+            }
             return false;
         }
     }
