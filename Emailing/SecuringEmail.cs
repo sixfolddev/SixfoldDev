221d83b76758dd67ab3ad7c4aa7765c6e210c1ed
Djgione <thegiones13@sbcglobal.net>
2019-11-26 16:24:06 -0800

    removed unnecessary dependencies and created separate mailing project

---
+++ Emailing/SecuringEmail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Google.Apis.Auth.OAuth2.Mvc;
+
+namespace Emailing
+{
+    public class SecuringEmail
+    {
+
+    }
+}
