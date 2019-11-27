ee14734443d21718ef8e214ebe044d7ec4d48487
Djgione <thegiones13@sbcglobal.net>
2019-11-26 16:26:20 -0800

    fixed dependencies further

--- Emailing/SecuringEmail.cs
+++ Emailing/SecuringEmail.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Google.Apis.Auth.OAuth2.Mvc;
-
+using Google.Apis.Auth.OAuth2;
 namespace Emailing
 {
     public class SecuringEmail
