af3c4b915fc72318f025b814b2d6492d60e7432a
Kohvu <44853020+Kohvu@users.noreply.github.com>
2019-12-03 15:04:40 -0800

    Updates to AuthN

---
+++ Authentication/Roomaid.Authentication Testing.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AuthenticationSystem;
+using System;
+
+
+namespace UnitTestAuthentication
+{
+    [TestClass]
+    public class UnitTestAuthN
+    {
+        Authentication authentication = new Authentication("tester01", "password1");
+
+        [TestMethod]
+        public void salt()
+        {
+            //Testing if salt gets retrieved
+            Assert.AreNotEqual(authentication.getRetrievedSalt(), "");
+        }
+        [TestMethod]
+        public void hashing()
+        {
+            //check if hashed pw is the same is the one stored to user ID in pw file
+            //If this passes, user is authenticated
+            Assert.AreEqual(authentication.generateHash(), authentication.dataStoreHash());
+        }
+
+
+    }
+}
