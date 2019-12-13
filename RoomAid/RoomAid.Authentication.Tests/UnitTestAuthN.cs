8253687d0189b25856733e47dbd5fcc80a2039d3
Michell <michellkuang96@yahoo.com>
2019-12-12 16:48:36 -0800

    updated authN files (Peter)

--- RoomAid/RoomAid.Authentication.Tests/UnitTestAuthN.cs
+++ RoomAid/RoomAid.Authentication.Tests/UnitTestAuthN.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RoomAid.Authentication;
-using System;
-
 
 namespace UnitTestAuthentication
 {
@@ -14,14 +12,14 @@ namespace UnitTestAuthentication
         public void salt()
         {
             //Testing if salt gets retrieved
-            Assert.AreNotEqual(authentication.getRetrievedSalt(), "");
+            Assert.AreNotEqual(authentication.GetRetrievedSalt(), "");
         }
         [TestMethod]
         public void hashing()
         {
             //check if hashed pw is the same is the one stored to user ID in pw file
             //If this passes, user is authenticated
-            Assert.AreEqual(authentication.generateHash(), authentication.dataStoreHash());
+            Assert.AreEqual(authentication.GenerateHash(), authentication.DataStoreHash());
         }
 
 
--- RoomAid/RoomAid.Authentication/Authentication.cs
+++ RoomAid/RoomAid.Authentication/Authentication.cs
@@ -1,71 +1,70 @@
 ﻿using System;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
-using Microsoft.AspNetCore.WebUtilities;
 using System.Text;
 
 namespace RoomAid.Authentication
 {
     public class Authentication
     {
-        private string retrievedSalt;
-        private string userID;
-        private string password;
-        private string hashed;
-        private bool authenticated;
+        private string _retrievedSalt;
+        private string _userID;
+        private string _password;
+        private string _hashed;
+        private bool _authenticated;
 
         //Create object when user tries to log in
         public Authentication(string userID, string password)
         {
-            this.userID = userID;
-            this.password = password;
-            retrievedSalt = "";
-            setRetrievedSalt();
-            generateHash();
-            authenticated = false;
+            this._userID = userID;
+            this._password = password;
+            _retrievedSalt = "";
+            SetRetrievedSalt();
+            GenerateHash();
+            _authenticated = false;
         }
 
         //Function that retrieves salt that is tied to user ID
         //If no salt exists, User ID doesn't exist
-        public void setRetrievedSalt()
+        public void SetRetrievedSalt()
         {
             try
             {
                 //pull salt from pw file
-                retrievedSalt = "AE2012DEWE193241"; //test salt
+                _retrievedSalt = "AE2012DEWE193241"; //test salt
             }
             catch (Exception)
             {
                 //Catch error handling.
                 //Returns error back to login screen if salt doesn't exist 
                 //which means account doesn't exist.
-                retrievedSalt = "";
+                _retrievedSalt = "";
             }
         }
 
-        public string getRetrievedSalt()
+        public string GetRetrievedSalt()
         {
-            return retrievedSalt;
+            return _retrievedSalt;
         }
 
         //Generate a hash with user entered pw and salt tied to user account
-        public string generateHash()
+        public string GenerateHash()
         {
             //Convert salt into byte array for hashing function
-            byte[] array = Encoding.ASCII.GetBytes(retrievedSalt);
+            byte[] array = Encoding.ASCII.GetBytes(_retrievedSalt);
 
-            hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password, //User input password when logging in
+            _hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: _password, //User input password when logging in
                 salt: array,        //Salt of user ID turned into byte[]
                 prf: KeyDerivationPrf.HMACSHA256,   //Derived Key Function
                 iterationCount: 100000,             //Iterations to slow down hashing
                 numBytesRequested: 256 / 8));       //32 bytes hash
 
-            return hashed;
+            return _hashed;
         }
 
         //Function to retrieve hashed password tied to a userID.
         //hashed pw not stored in variable for security reasons
-        public string dataStoreHash()
+        public string DataStoreHash()
         {
             string storedHash;
             try
@@ -84,46 +83,16 @@ namespace RoomAid.Authentication
         }
 
         //function to check if user ID and password that has been input is correct
-        public bool compareHashes()
+        public bool CompareHashes()
         {
-            if (hashed == dataStoreHash())
+            if (_hashed == DataStoreHash())
             {
-                authenticated = true;
+                _authenticated = true;
             }
             else
-                authenticated = false;
+                _authenticated = false;
 
-            return authenticated;
-        }
-
-        public string generateJWT()
-        {
-            //Json Web token
-            //Header, payload, signature
-            //const token = base64urlEncoding(header) + '.' + 
-            //base64urlEncoding(payload) + '.' + base64urlEncoding(signature)
-            if (authenticated == false)
-            {
-                string header, payload, key;
-                string token;
-                header = "{\n" +
-                            "   alg: HMAC256,\n" +
-                            "   typ: JWT\n" +
-                            "}";
-                payload = "{\n" +
-                            "   ID: ," + userID + "\n" +
-                            "}";
-                key = "tempkey";
-
-                token = WebEncoders.Base64UrlEncode(Encoding.ASCII.GetBytes(header)) + "." +
-                    WebEncoders.Base64UrlEncode(Encoding.ASCII.GetBytes(payload)) + "." +
-                    WebEncoders.Base64UrlEncode(Encoding.ASCII.GetBytes(key));
-
-                return token;
-            }
-            else
-                return "";
-
+            return _authenticated;
         }
     }
 }
