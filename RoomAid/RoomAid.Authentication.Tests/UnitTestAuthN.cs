7315b531d8e63365ba9c2b67f6c52e55902cfb6e
Kohvu <44853020+Kohvu@users.noreply.github.com>
2020-01-21 01:50:48 -0800

    Modified test to fit new AuthN

--- RoomAid/RoomAid.Authentication.Tests/UnitTestAuthN.cs
+++ RoomAid/RoomAid.Authentication.Tests/UnitTestAuthN.cs
@@ -9,17 +9,11 @@ namespace UnitTestAuthentication
         Authentication authentication = new Authentication("tester01", "password1");
 
         [TestMethod]
-        public void salt()
-        {
-            //Testing if salt gets retrieved
-            Assert.AreNotEqual(authentication.GetRetrievedSalt(), "");
-        }
-        [TestMethod]
         public void hashing()
         {
             //check if hashed pw is the same is the one stored to user ID in pw file
             //If this passes, user is authenticated
-            Assert.AreEqual(authentication.GenerateHash(), authentication.DataStoreHash());
+            Assert.IsTrue(authentication.GetAuthenticated());
         }
 
 
