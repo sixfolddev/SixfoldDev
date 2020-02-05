027058e82732258868b325f3ae642eddb6840e77
Jacen Tan <43791736+ReticentDesign@users.noreply.github.com>
2020-02-04 18:59:12 -0800

    Initial Commit
    Deleted old AuthZ folder.
    Replaced with RoomAid.Authorization
    Created AuthZClaims
    Created IAuthZService
    Created AuthZService

---
+++ RoomAid/Authorization/AuthZClaims.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization
+{
+    class AuthZClaims
+    {
+        public enum AuthZEnum
+        {
+            None,
+            EnabledAccount,
+            CreateAdmin,
+            EnableACcount,
+            DisableACcount,
+            EditProfile,
+            ViewProfile,
+            DeleteAccount,
+            SearchHousehold,
+            SendMessage,
+            ReplyMessage,
+            ViewMessage,
+            MarkMessage,
+            DeleteMessage,
+            SendInvite,
+            ViewInvite,
+            AcceptInvite,
+            DeclineInvite,
+            ViewTenant,
+            PromoteTenant,
+            DemoteTenant,
+            RemoveTenant,
+            LeaveHousehold,
+            CreateExpense,
+            ViewExpense,
+            EditExpense,
+            DeleteExpense,
+            CreateTask,
+            ViewTask,
+            EditTask,
+            DeleteTask,
+            CreateSupplyRequest,
+            ViewSupplyRequest,
+            EditSupplyRequest,
+            DeleteSupplyRequest
+        }
+
+        public string UserID { get; }
+        public string HouseholdID { get; }
+        public AuthZEnum[] Claims { get; }
+
+        
+    }
+}
---
+++ RoomAid/Authorization/AuthZService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization
+{
+    class AuthZService : IAuthZService
+    {
+        public bool Authorize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
